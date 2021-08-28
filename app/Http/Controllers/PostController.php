cda8b49993d56012e18b8468030c89aa23a3c0ef
scar0125 <ryoga.yoshisato@gmail.com>
2021-08-29 02:26:07 +0900

    add 検索機能

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,9 +7,82 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
+    //検索画面
+    
+    public function search(Request $request) {
+      $keyword_name = $request->name;
+      $keyword_body = $request->body;
+
+      if(!empty($keyword_name) && empty($keyword_body)) {
+      $query = Post::query();
+      $posts = $query->where('name','like', "%" .$keyword_name. "%")->get();
+      $message = "「". $keyword_name."」を含む名前の検索が完了しました。";
+      return view('search')->with([
+        'posts' => $posts,
+        'message' => $message,
+      ]);
+    }
+    elseif(empty($keyword_name) && !empty($keyword_body)) {
+      $query = Post::query();
+      $posts = $query->where('name','like', "%" .$keyword_body. "%")->get();
+      $message = "「". $keyword_body."」を含む本文の検索が完了しました。";
+      return view('search')->with([
+        'posts' => $posts,
+        'message' => $message,
+      ]);
+    }
+    else {
+      $message = "検索結果はありません。";
+      return view('search')->with('message',$message);
+      }
+    }
+    
+    
+    
+    
+    
+    
+    
     //Post一覧の表示
     public function index(Post $post)
     {
-        return $post->get();
+        return view('index')->with(['posts' => $post->getPaginateByLimit()]);
     }
+    
+    
+    public function show(Post $post)
+    {
+        return view('show')->with(['post' => $post]);
+    }
+    
+    public function create()
+    {
+        return view('create');
+    }
+    
+    public function store(Post $post, PostRequest $request)
+    {
+        $input = $request['post'];
+        $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
+    }
+    
+    public function edit(Post $post)
+    {
+        return view('edit')->with(['post' => $post]);
+    }
+    
+    public function update(PostRequest $request, Post $post)
+    {
+        $input_post = $request['post'];
+        $post->fill($input_post)->save();
+        return redirect('/posts/' . $post->id);
+    }
+    
+    public function delete(Post $post)
+    {
+        $post->delete();
+        return redirect('/');
+    }
+    
 }
---
+++ resources/views/index.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>Blog</title>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    </head>
+    
+    <body>
+        <h1>検索条件を入力</h1>
+    <form action="/search" method="post">
+      @csrf
+      @method('GET')
+    
+    <div class="form-group">
+    <label>フィールド名</label>
+    <input type="text" class="form-control col-md-5" placeholder="検索したい名前を入力してください" name="name">
+    </div>
+    
+    <div class="form-group">
+      <label>本文</label>
+      <input type="text" class="form-control col-md-5" placeholder="本文を入力してください" name="body" value="{{ old("body")}}">
+    </div>
+
+    <button type="submit" class="btn btn-primary col-md-5">検索</button>
+    </form>
+    
+    @if(session('flash_message'))
+    <div class="alert alert-primary" role="alert" style="margin-top:50px;">{{ session('flash_message')}}</div>
+    @endif
+    
+    <div class='posts'>
+        @foreach ($posts as $post)
+        <div class='post'>
+            <h2 class='name'>{{ $post->name }}</h2>
+            <p class='body'>{{ $post->body }}</p>
+        </div>
+        @endforeach
+    </div>
+    <div class='paginate'>
+        {{ $posts->links() }}
+    </div>
+    
+    </body>
+</html>
